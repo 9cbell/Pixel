2001a6938262db2f16d120be8d8dca425610b3fb
9cbell <31480938+9cbell@users.noreply.github.com>
2017-11-08 09:57:59 -0600

    Add files via upload

--- pixel.js
+++ pixel.js
@@ -49,6 +49,31 @@ function Pixel(r,g,b){
         this.red = this.red*(1 + n)
         this.green = this.green*(1 + n)
         this.blue = this.blue*(1 + n)
+
+        if(this.red < 0){
+          this.red = 0;
+        }
+
+        else if(this.red > 255){
+         this.red = 255;
+        }
+
+        if(this.green < 0){
+          this.green = 0;
+        }
+
+        else if(this.green > 255){
+          this.green = 255
+        }
+
+        if(this.blue < 0){
+          this.blue = 0;
+        }
+
+        else if(this.blue > 255){
+          this.blue = 255;
+        }
+
     };
 
     this.complement = function(n){
